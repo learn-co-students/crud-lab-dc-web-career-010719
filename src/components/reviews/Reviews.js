97d7edfd1ac9c107136e39e769f7091beb412b1c
dcjones1 <dcejjones@gmail.com>
2019-03-28 20:42:25 -0400

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,10 +3,22 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const { reviews, deleteReview } = this.props
     return (
-      <ul>
-        Reviews
-      </ul>
+      <React.Fragment>
+        <ul>
+          <h4>Reviews:</h4>
+          {reviews.map((review) => {
+            return (
+              <Review
+                key={review.id}
+                review={review}
+                deleteReview={deleteReview}
+              />
+            )})}
+        </ul>
+        <br/>
+      </React.Fragment>
     );
   }
 };
--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react'
 import ReviewInput from '../components/reviews/ReviewInput'
 import Reviews from '../components/reviews/Reviews'
+import { connect } from 'react-redux'
 
 class ReviewsContainer extends Component {
 
+  showReviews = () => {
+    return this.props.reviews.filter((review) => {
+      return review.restaurantId === this.props.restaurantId
+    })
+  }
+
+
   render() {
+    const { addReview, deleteReview, restaurantId } = this.props
     return (
       <div>
-        <ReviewInput />
-        <Reviews />
+        <ReviewInput
+          addReview={addReview}
+          restaurantId={restaurantId}
+        />
+        <Reviews
+          reviews={this.showReviews()}
+          deleteReview={deleteReview}
+        />
       </div>
     )
   }
 }
 
-export default ReviewsContainer
+const mapStateToProps = (state) => {
+  return {
+    reviews: state.reviews
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addReview: (review) => dispatch({ type: 'ADD_REVIEW', review }),
+    deleteReview: (id) => dispatch({ type: 'DELETE_REVIEW', id })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewsContainer)
